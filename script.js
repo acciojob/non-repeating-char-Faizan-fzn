465e63969ee912bed5d0d95ff06094b98f8bf867
Faizan Ahmed <153298469+Faizan-fzn@users.noreply.github.com>
2024-01-20 01:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function firstNonRepeatedChar(str) {
 	for(let i = 0; i < str.length; i++){
 		let cnt = 0;
 		for(let j = 0; j < str.length; j++){
-		if(str.charAt(i) === str.charAt(i+1)){
+		if(str.charAt(i) === str.charAt(j)){
 			cnt++;
 		}
 	}
